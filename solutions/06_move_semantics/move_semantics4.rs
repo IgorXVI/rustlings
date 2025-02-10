a539b6c569e66f2d645eda154b3e4c6bc6f8e6ed
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2025-02-10 02:14:08 -0300

    inicio structs

--- solutions/06_move_semantics/move_semantics4.rs
+++ solutions/06_move_semantics/move_semantics4.rs
@@ -9,13 +9,13 @@ mod tests {
     #[test]
     fn move_semantics4() {
         let mut x = Vec::new();
-
         let y = &mut x;
+        // `y` used here.
         y.push(42);
-
+        // The mutable reference `y` is not used anymore,
+        // therefore a new reference can be created.
         let z = &mut x;
         z.push(13);
-
         assert_eq!(x, [42, 13]);
     }
 }
--- solutions/quizzes/quiz1.rs
+++ solutions/quizzes/quiz1.rs
@@ -1,22 +1,13 @@
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-// - If
-//
 // Mary is buying apples. The price of an apple is calculated as follows:
 // - An apple costs 2 rustbucks.
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
-
-fn calculate_price_of_apples(amount: i32) -> i32 {
-    if (amount > 40) {
-        return amount;
+fn calculate_price_of_apples(n_apples: u64) -> u64 {
+    if n_apples > 40 {
+        n_apples
     } else {
-        return amount * 2;
+        2 * n_apples
     }
 }
 
